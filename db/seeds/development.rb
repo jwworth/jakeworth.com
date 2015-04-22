7b484edf7698ec07e64fd0a56e44db56126b1197
Jake Worth <jakeworth82@gmail.com>
2015-04-22 18:26:35 -0400

    Seed the posts in development

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,4 +1,12 @@
 unless Developer.find_by_email('jake@example.com')
-  puts 'Creating Jake'
   Developer.create!(email: 'jake@example.com', password: 'password')
+  puts 'Created Jake'
+end
+
+technologies = %w(assembly bash clojure data ember)
+
+10.times do
+  theme = technologies.sample
+  post = Post.create!(title: "A post about #{theme}", body: theme * 200)
+  puts "Created post ##{post.id}"
 end
