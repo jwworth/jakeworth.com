8bc857bac3426b409b3a5a81b1b37673cf48c798
Jake Worth <jakeworth82@gmail.com>
2015-04-24 23:11:28 -0500

    Update routes to support legacy posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, param: :url_slug
+  get '/sessions/login', to: 'sessions#new', as: 'new_session'
+  post '/sessions/login', to: 'sessions#create'
+  get '/sessions/logout', to: 'sessions#destroy', as: 'destroy_session'
 
-  get '/login', to: 'sessions#new', as: 'new_session'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get '/:url_slug', to: 'posts#show', as: 'post', param: :url_slug
+  get '/:url_slug/edit', to: 'posts#edit', as: 'edit_post', param: :url_slug
+  patch '/:url_slug', to: 'posts#update', param: :url_slug
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts/new', to: 'posts#create', as: 'posts'
 end
