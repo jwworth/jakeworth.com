c34381b4bb624de2007e9fffcb5899efb0afd9cc
Jake Worth <jakeworth82@gmail.com>
2015-04-28 21:51:32 -0500

    Make sessions routes a little easier to type

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  get '/sessions/login', to: 'sessions#new', as: 'new_session'
-  post '/sessions/login', to: 'sessions#create'
-  get '/sessions/logout', to: 'sessions#destroy', as: 'destroy_session'
+  get '/s/login', to: 'sessions#new', as: 'new_session'
+  post '/s/login', to: 'sessions#create'
+  get '/s/logout', to: 'sessions#destroy', as: 'destroy_session'
 
   get '/:url_slug', to: 'posts#show', as: 'post', param: :url_slug
   get '/:url_slug/edit', to: 'posts#edit', as: 'edit_post', param: :url_slug
   patch '/:url_slug', to: 'posts#update', param: :url_slug
+
   get '/posts/new', to: 'posts#new', as: 'new_post'
   post '/posts/new', to: 'posts#create', as: 'posts'
   post '/post_preview/', to: 'posts#preview'
