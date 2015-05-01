1de8cff7d5f2aab8c4d10b02e352b62452f5b039
Jake Worth <jakeworth82@gmail.com>
2015-05-01 15:42:11 -0500

    Install basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   authem_for :developer
 
+  if ENV['http_auth']
+    http_basic_authenticate_with name: ENV['http_name'], password: ENV['http_password']
+  end
+
   private
 
   def require_developer
