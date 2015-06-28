28c36c13be29a9963a01cc7cb3fe13dfa93bf542
Jake Worth <jakeworth82@gmail.com>
2015-06-28 12:26:27 -0500

    Protect db from nulls

---
+++ db/migrate/20150628171109_add_constraints_to_posts_and_developers.rb
@@ -0,0 +1,10 @@
+class AddConstraintsToPostsAndDevelopers < ActiveRecord::Migration
+  def change
+    change_column_null :developers, :created_at, false
+    change_column_null :developers, :updated_at, false
+    change_column_null :posts, :created_at, false
+    change_column_null :posts, :updated_at, false
+    change_column_null :posts, :title, false
+    change_column_null :posts, :body, false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150423152139) do
+ActiveRecord::Schema.define(version: 20150628171109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,15 +34,15 @@ ActiveRecord::Schema.define(version: 20150423152139) do
     t.string   "email",                           null: false
     t.string   "password_digest",                 null: false
     t.string   "password_reset_token", limit: 60, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "url_slug"
   end
 
