750e6721acf597d9708f264fba2dd62b60f9b510
Jake Worth <jakeworth82@gmail.com>
2016-01-09 17:15:16 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,13 @@ puts 'Creating projects'
 Project.find_or_create_by(title: 'jakeworth.com') do |project|
   project.description = 'My development blog.'
   project.hyperlink = 'http://jakeworth.com'
-  project.featured = true
+  project.featured_order = 1
 end
 
 Project.find_or_create_by(title: 'CERAMIC NATION') do |project|
   project.description = 'A Markov Chain generated novel.'
   project.hyperlink = 'http://ceramic-nation.jakeworth.com'
-  project.featured = true
+  project.featured_order = 2
 end
 
 Project.find_or_create_by(title: 'Today I Learned') do |project|
@@ -22,7 +22,7 @@ Project.find_or_create_by(title: 'Today I Learned') do |project|
     TIL is a project by Hashrocket to catalogue the sharing & accumulation of knowledge as it happens day-to-day. Posts have a 200-word limit and any Hashrocket team member can contribute. This was a project I built while pair-programming as an apprentice at Hashrocket.
     DESC
   project.hyperlink = 'http://til.hashrocket.com'
-  project.featured = true
+  project.featured_order = 3
 end
 
 Project.find_or_create_by(title: 'Capybara Driver Benchmarking') do |project|
@@ -30,6 +30,6 @@ Project.find_or_create_by(title: 'Capybara Driver Benchmarking') do |project|
     This is the test app I built for a talk at Hashrocket's Summer Miniconf 2015. Its purpose is to benchmark the performance of the drivers available for use with Capybara.
     DESC
   project.hyperlink = 'https://github.com/jwworth/cap-driver-benchmarking'
-  project.featured = true
+  project.featured_order = 4
 end
 puts '...complete'
