f4bde1b00282d5ab1c6895d44590f1eaec8a9f34
Jake Worth <jake@jakeworth.com>
2016-12-29 10:07:57 -0600

    Add results of `rails app:update`

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,15 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module WorthChicagoCo
   class Application < Rails::Application
-    config.active_record.schema_format = :sql
-    config.time_zone = 'Central Time (US & Canada)'
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_worth-chicago_co_session'
+Rails.application.config.session_store :cookie_store, key: '_worth_chicago_co_session'
