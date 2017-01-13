85ec1f2b46b1849ca5fcdec131cd57073cf86446
Jake Worth <jake@jakeworth.com>
2017-01-12 22:20:17 -0600

    Set the time zone to my time zone

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,7 @@ Bundler.require(*Rails.groups)
 
 module WorthChicagoCo
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    config.time_zone = 'Central Time (US & Canada)'
     config.active_record.schema_format = :sql
   end
 end
