9c18fec681366b303f975d8ac9c44cade521af9d
Jake Worth <jake@jakeworth.com>
2017-02-25 00:49:28 -0600

    Allow hyperlink to be nil

--- app/models/speaking_engagement.rb
+++ app/models/speaking_engagement.rb
@@ -1,4 +1,8 @@
 class SpeakingEngagement < ApplicationRecord
   validates_presence_of :title, :location, :date
   scope :by_date, -> { order('date desc') }
+
+  def hyperlink=(hyperlink)
+    self[:hyperlink] = hyperlink.presence
+  end
 end
